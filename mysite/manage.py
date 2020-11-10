5a8bd4c548d5a0bfc721f5e4b50cfd1ea4b990c6
vidaleshin <vidalexxoxdd@gmail.com>
2020-11-09 22:16:21 -0300

    cambio de comentario

--- mysite/manage.py
+++ mysite/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks asdasdsa."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
