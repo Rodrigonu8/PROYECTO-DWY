103a63f8710ef5da0d6717122c2ef367fbd974af
Rodrigo nuñez <rodrigonu8@gmail.com>
2020-11-10 01:08:41 -0300

    Creacion de clases Bicicleta y Portico

--- mysite/apps/Registro/models.py
+++ mysite/apps/Registro/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Bicicleta(models.Model):
+    id_bicicleta = models.IntegerField(primary_key=True)
+    modelo = models.CharField(max_length=50)
+    candado = models.BooleanField(default=False)
+    fecha_ingreso = models.DateField()
+
+    def __str__(self):
+        return self.id_bicicleta
+
+
+class Portico(models.Model):
+    id_portico = models.IntegerField()
+    id_bicicleta = models.ForeignKey(Bicicleta,null=False, on_delete=models.CASCADE)
+    ubicacion = models.TextField()
+
+    def __str__(self):
+        return self.id_bicicleta
+
+
